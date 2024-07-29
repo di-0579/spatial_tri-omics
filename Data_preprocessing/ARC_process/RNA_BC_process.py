37dca689312e2acbae23ab28dcf28759e9e9d239
di-0579 <98048099+di-0579@users.noreply.github.com>
2024-07-29 00:27:31 -0400

    Update RNA_BC_process.py

--- Data_preprocessing/ARC_process/RNA_BC_process.py
+++ Data_preprocessing/ARC_process/RNA_BC_process.py
@@ -14,6 +14,6 @@ output_filename = args["output"]
 with gzopen(input_filename, "rt") as in_handle:
     with open(output_filename, "w") as out_handle:
         for title, seq, qual in FastqGeneralIterator(in_handle):
-            new_seq = seq[22:30] + seq[60:68] + seq[98:108]  # BC2 + BC1 + UMI
+            new_seq = seq[22:30] + seq[60:68] + seq[98:108]  
             new_qual = qual[22:30] + qual[60:68] + qual[98:108]
             out_handle.write("@%s\n%s\n+\n%s\n" % (title, new_seq, new_qual))
